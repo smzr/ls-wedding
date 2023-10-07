a9cadfbb56d2aa11e0dbb6877899f2d036ec5ad4
smzr <sammy@smzr.co.uk>
2023-10-07 21:07:52 +0100

    SSR fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
       attendeeList: 'storyblok/AttendeeList'
     }
   })],
-  output: import.meta.env.VITE_ENVIRONMENT  ===  'preview'  ?  'hybrid'  :  'static',
+  output: import.meta.env.VITE_ENVIRONMENT  ===  'preview'  ?  'server'  :  'hybrid',
   adapter: import.meta.env.VITE_ENVIRONMENT === 'preview' ? netlify() : undefined,
   site: 'https://lukeandsam.netlify.app'
 });
